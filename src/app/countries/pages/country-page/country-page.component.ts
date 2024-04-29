36ce6f66e3d1537958d1eb1c984e2cfb0c8f0245
Jose Luis <bosito321@gmail.com>
2024-04-28 19:22:20 -0500

    create country page info

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { switchMap, take } from 'rxjs';
+import { CountriesService } from '../../services/countries.service';
+import { ICountry } from '../../interfaces/by-capital.interface';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styleUrls: ['./country-page.component.css'],
 })
-export class CountryPageComponent {}
+export class CountryPageComponent implements OnInit {
+  public country?: ICountry;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router
+  ) {}
+
+  public ngOnInit(): void {
+    this.searchCountry();
+  }
+
+  private searchCountry() {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }: Params) => {
+          return this.countriesService.searchCountryByAlphaCode(id);
+        })
+      )
+      .subscribe({
+        next: (country: ICountry | null) => {
+          if (!country) {
+            this.router.navigate(['']);
+            return;
+          }
+
+          this.country = country;
+        },
+      });
+  }
+}
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { ICountry } from '../interfaces/by-capital.interface';
 
 @Injectable({
@@ -34,6 +34,16 @@ export class CountriesService {
       catchError((error) => {
         console.error(error);
         return of([]);
+      })
+    );
+  }
+
+  public searchCountryByAlphaCode(code: string): Observable<ICountry | null> {
+    return this.http.get<ICountry[]>(`${this.apiUrl}/alpha/${code}`).pipe(
+      map((countries) => (countries.length > 0 ? countries[0] : null)),
+      catchError((error) => {
+        console.error(error);
+        return of(null);
       })
     );
   }
